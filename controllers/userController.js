713b204b57a36fbfee1e195bee5f2151801fc878
Ox Noj <geneveva@gmail.com>
2023-05-21 21:08:29 -0700

    Updates to user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const { User } = require('../models');
+const { User, Thought } = require('../models');
 
 module.exports = {
     //Get all users
@@ -13,7 +13,7 @@ module.exports = {
             .catch((err) => {
             console.log(err);
             return res.status(500).json(err);
-        })
+        });
     },
     //Get a single user
     getSingleUser(req,res) {
@@ -30,13 +30,38 @@ module.exports = {
             console.log(err);
             return res.status(500).json(err);
         });
-    }
-}
+    },
+    //Create a user
+    createUser(req, res) {
+        User.create(req.body)
+        .then((user) => res.json(User))
+        .catch((err) => res.status(500).json(err));
+    },
+    //Update a user
+    updateUser(req, res) {
+        User.findOneAndUpdate
+    },
+    //Remove a user
+    deleteUser(req, res) {
+        User.findOneAndDelete({ _id: req.params.userId })
+        .then((user) =>
+        !user
+            ? res.status(404).json({ message: 'There is no User with that ID'})
+            : Thought.deleteMany({ _id: { $in: user.thoughts } })
+            )
+            .then(() => res.json({ message: 'The User and Thought are deleted' }))
+            .catch((err) => res.status(500).json(err));
+    },
+    },
+    //Add a friend
+    },
+    //Remove a friend
+};
 
 //get all users - X
 //get single user - X
-//create a user
+//create a user - X
 //update a user
-//remove a user
+//remove a user - X
 //add a friend
 //remove a friend
