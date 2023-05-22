b7be696ba6136f337804904e4ffbfca3d06daff6
Ox Noj <geneveva@gmail.com>
2023-05-21 19:27:35 -0700

    fixing models

---
+++ models/Reaction.js
@@ -0,0 +1,32 @@
+const { Schema, Types } = require('mongoose'); //Schema only
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.ObjectId,
+            default: () => new Schema.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        }
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+module.exports = reactionSchema;
--- models/User.js
+++ models/User.js
@@ -35,6 +35,9 @@ const userSchema = new Schema(
         id: false,
     }
 );
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
 
 const User = model('User', userSchema);
 
