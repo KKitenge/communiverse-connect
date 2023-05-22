af64b5bb6089d83c00db044352a3fbd011eb8b54
Ox Noj <geneveva@gmail.com>
2023-05-21 21:33:51 -0700

    Edited user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,57 +11,91 @@ module.exports = {
                 return res.json(userObj);
             })
             .catch((err) => {
-            console.log(err);
-            return res.status(500).json(err);
-        });
+                console.log(err);
+                return res.status(500).json(err);
+            });
     },
     //Get a single user
-    getSingleUser(req,res) {
+    getSingleUser(req, res) {
         User.findOne({ _id: req.params.userId })
-        .select('-__v')
-        .then(async (user) =>
-        !user
-            ? res.status(404).json({ message: 'There is no student with that ID'})
-            : res.json({
-                user
-            })            
-        )
-        .catch((err) => {
-            console.log(err);
-            return res.status(500).json(err);
-        });
+            .select('-__v')
+            .then(async (user) =>
+                !user
+                    ? res.status(404).json({ message: 'There is no student with that ID' })
+                    : res.json({
+                        user
+                    })
+            )
+            .catch((err) => {
+                console.log(err);
+                return res.status(500).json(err);
+            });
     },
     //Create a user
     createUser(req, res) {
         User.create(req.body)
-        .then((user) => res.json(User))
-        .catch((err) => res.status(500).json(err));
+            .then((user) => res.json(user))
+            .catch((err) => res.status(500).json(err));
     },
     //Update a user
     updateUser(req, res) {
-        User.findOneAndUpdate
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $set: req.body },
+            { runValidators: true, new: true }
+        )
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: 'There is no User with that ID' })
+                    : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
     },
     //Remove a user
     deleteUser(req, res) {
         User.findOneAndDelete({ _id: req.params.userId })
-        .then((user) =>
-        !user
-            ? res.status(404).json({ message: 'There is no User with that ID'})
-            : Thought.deleteMany({ _id: { $in: user.thoughts } })
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: 'There is no User with that ID' })
+                    : Thought.deleteMany({ _id: { $in: user.thoughts } })
             )
             .then(() => res.json({ message: 'The User and Thought are deleted' }))
             .catch((err) => res.status(500).json(err));
     },
-    },
     //Add a friend
+    addFriend(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $addToSet: { friends: req.body } },
+            { runValidators: true, new: true }
+        )
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: 'There is no User with that ID' })
+                    : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
     },
     //Remove a friend
+    removeFriend(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $pull: { friends: { friendsId: req.params.friendID } } },
+            { runValidators: true, new: true }
+        )
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: 'There is no User with that ID' })
+                    : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
 };
 
 //get all users - X
 //get single user - X
 //create a user - X
-//update a user
+//update a user - X
 //remove a user - X
-//add a friend
-//remove a friend
+//add a friend - X
+//remove a friend - X
