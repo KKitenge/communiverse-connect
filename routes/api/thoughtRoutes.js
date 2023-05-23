4e30f4e733baff7feeae04e51bd38892fba58029
Ox Noj <geneveva@gmail.com>
2023-05-22 17:43:44 -0700

    created and edited routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  createReaction,
+  deleteReaction,
+} = require('../../controllers/thoughtController.js');
+
+router.route('/').get(getThoughts).post(createThought);
+
+router.route('/:thoughtId').get(getSingleThought).delete(deleteThought).put(updateThought);
+
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
+
+router.route('/:thoughtId/reactions').post(createReaction);
+
+module.exports = router;
